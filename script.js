aa0f2758f50cc44a0a9f38df90d4295ac091324a
Nobel Alkaid <nobeliumacc@gmail.com>
2023-03-10 14:41:04 +0100

    Picture in picture implementation

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('testing')
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt the user to select a media stream, pass to the video element , then play
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log('Whoopes, error here', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable the button
+    button.disabled = true;
+    // Start picture in picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+// On loaad
+selectMediaStream();
